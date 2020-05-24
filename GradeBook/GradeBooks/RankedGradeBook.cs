734681c4f3c80b310042aaa76690e60437d95b4d
lcleonp <lcleonp@hotmail.com>
2020-05-24 11:03:21 -0700

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,8 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
@@ -7,5 +11,61 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+
+            if (Students.Count > 4)
+            {
+                List<double> sortedGrades = getSortedGrades();
+                var n = Convert.ToInt32(Math.Round(sortedGrades.Count * 0.2)) - 1;
+
+                if (averageGrade >= sortedGrades[n])
+                    return 'A';
+                else if (averageGrade >= sortedGrades[n + 1])
+                    return 'B';
+                else if (averageGrade >= sortedGrades[n + 2])
+                    return 'C';
+                else if (averageGrade >= sortedGrades[n + 3])
+                    return 'D';
+                else
+                    return 'F';
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
+        }
+
+        private List<double> getSortedGrades()
+        {
+            List<double> grades = new List<double>();
+            if (Students.Count > 0)
+            {
+                foreach (var student in Students)
+                {
+                    grades.Add(student.AverageGrade);
+                }
+                grades = grades.OrderBy(o => o).ToList();
+            }
+
+            return grades;
+
+        }
+
+        private double getMaxGrade()
+        {
+            double max = 0;
+            foreach (Student student in Students)
+            {
+                if (student.AverageGrade > max) max = student.AverageGrade;
+
+            }
+
+            return max;
+        }
+
     }
 }
